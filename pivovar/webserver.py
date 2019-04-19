67049b9d4b1541be4a763fdeda6878d6fd1045d0
Jaroslav Henner <jhenner@redhat.com>
2019-04-19 20:08:47 +0200

    Add l18n using babel and gettext.

--- pivovar/webserver.py
+++ pivovar/webserver.py
@@ -1,5 +1,6 @@
 from pivovar import config
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+from flask_babel import Babel
 from urllib.parse import urljoin
 
 
@@ -7,10 +8,21 @@ class DefaultConfig(object):
     PORT = 5000
     WASH_URL = 'http://localhost:5001/'
     INSTANCE_CONFIG_FILE = 'webserver.cfg'
+    BABEL_TRANSLATION_DIRECTORIES = 'translations'
 
 
 app = Flask(__name__)
 config.configure_app(app)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    # Try to guess the language from the user accept
+    # header the browser transmits.  We support en/cs in this
+    # example.  The best match wins.
+    loc = request.accept_languages.best_match(['en', 'cs'])
+    return loc
 
 
 @app.route('/<path:filename>')
--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
-
+import os
+import subprocess
 from setuptools import setup
+
+
+# https://stackoverflow.com/a/37906830/1950100
+def create_mo_files():
+    data_files = []
+    localedir = 'pivovar/translations'
+    po_dirs = [localedir + '/' + l + '/LC_MESSAGES/'
+               for l in next(os.walk(localedir))[1]]
+    for d in po_dirs:
+        mo_files = []
+        po_files = [f
+                    for f in next(os.walk(d))[2]
+                    if os.path.splitext(f)[1] == '.po']
+        for po_file in po_files:
+            filename, extension = os.path.splitext(po_file)
+            mo_file = filename + '.mo'
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(d + po_file, d + mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo_files.append(d + mo_file)
+        data_files.append((d, mo_files))
+    return data_files
+
 
 setup(
     setup_requires=['pbr'],
     pbr=True,
+    data_files=create_mo_files()
 )
