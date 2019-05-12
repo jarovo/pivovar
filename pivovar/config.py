d30fbca8bc3b9205b97e744b99992f68def87aa4
Jaroslav Henner <jhenner@redhat.com>
2019-05-12 18:07:30 +0200

    Make the default pivovar config file loaded from path relative to it.

--- pivovar/config.py
+++ pivovar/config.py
@@ -1,10 +1,13 @@
 import configparser as _configparser
+import os.path
+import pivovar
 import sys
 
 
 def _get_cfg():
     _cfg = _configparser.ConfigParser()
-    _cfg.read_file(open('pivovar.cfg'))
+    pivovar_pkg_dirname = os.path.dirname(pivovar.__file__)
+    _cfg.read_file(open(os.path.join(pivovar_pkg_dirname, 'pivovar.cfg')))
     return _cfg
 
 
