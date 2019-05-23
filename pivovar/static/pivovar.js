14a3717926996f6a87ab2a0a7336cedc6628c09e
Jaroslav Henner <jhenner@redhat.com>
2019-05-23 13:15:12 +0200

    Make wash phases sortable.

--- pivovar/static/pivovar.js
+++ pivovar/static/pivovar.js
@@ -24,6 +24,7 @@ const i18n = new VueI18n({
   messages, // set locale messages
 })
 
+
 fetch(wm_url + '/wash_machine')
     .then(response => response.json())
     .catch(error => console.error('Error:', error))
@@ -73,9 +74,9 @@ Vue.component('wash-machine', {
       <h3>{{ $t("Keg Washer") }} {{wm_name}}</h3>
       <div class="col-sm-3">
         <h3>{{ $t("Phases") }}</h3>
-        <div class="list-group" id="phases">
+        <ol id="phases" class="list-group" >
           <wash-machine-phase v-for="phase_name in phases" :wm_id="wm_name" :phase_name="phase_name"></wash-machine-phase>
-        </div>
+        </ol>
       </div>
       <div class="col-sm-9">
         <h3>{{ $t("water temp in time") }}</h3>
@@ -95,12 +96,51 @@ Vue.component('wash-machine', {
             console.log(val, oldVal)
             update_temp_log(this.wm_name, val)
         },
+    },
+
+    mounted () {
+        var adjustment;
+
+        $("#phases").sortable({
+            handle: 'i.icon-move',
+            placeholder: '<li class="placeholder list-group-item"></li>',
+            onDrop: function ($item, container, _super) {
+                var $clonedItem = $('<li/>').css({height: 0});
+                $item.before($clonedItem);
+                $clonedItem.animate({'height': $item.height()});
+
+                $item.animate($clonedItem.position(), function  () {
+                    $clonedItem.detach();
+                    _super($item, container);
+                });
+            },
+
+            // set $item relative to cursor position
+            onDragStart: function ($item, container, _super) {
+                var offset = $item.offset(),
+                    pointer = container.rootGroup.pointer;
+
+                adjustment = {
+                    left: pointer.left - offset.left,
+                    top: pointer.top - offset.top
+                };
+
+                _super($item, container);
+            },
+
+            onDrag: function ($item, position) {
+                $item.css({
+                    left: position.left - adjustment.left,
+                    top: position.top - adjustment.top
+                });
+            }
+        });
     }
 });
 
 Vue.component('wash-machine-phase', {
     props: ['wm_id', 'phase_name'],
-    template: '<a class="list-group-item">{{ phase_name }}</a>',
+    template: '<li class="list-group-item"><i class="icon-move glyphicon glyphicon-move"></i>{{ phase_name }}</li>',
 });
 
 const WashMachines = {
