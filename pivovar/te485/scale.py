1a35c87a3a054f1b5f63156599a6d100cc31cb1a
Jaroslav Henner <jhenner@redhat.com>
2019-07-14 19:52:33 +0200

    Add files for supporting Papouch te485 measuring interface.

---
+++ pivovar/te485/scale.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+from time import sleep
+import ctypes
+from six.moves import input
+import requests
+from urllib.parse import urljoin
+
+full_cal_value = 1.
+
+
+class Scale(object):
+    def calibrate(self, raw_zero, raw_full, full_value):
+        self.raw_zero = raw_zero
+        self.raw_full = raw_full
+        self.full_value = full_value
+
+    def load(self, raw):
+        return ((raw - self.raw_zero)
+                * self.full_value / (self.raw_full - self.raw_zero))
+
+    def __str__(self):
+        return (
+            'Scale(raw_zero: {:d}, raw_full: {:d}, full_value: {})'
+        ).format(self.raw_zero, self.raw_full, self.full_value)
+
+
+def evok_read(evok_baseurl):
+    register_url = urljoin(evok_baseurl, '/rest/register')
+    raw = requests.get(f'{register_url}/UART_49_3_2_inp').json()['value']
+    cal = requests.get(f'{register_url}/UART_49_3_1_inp').json()['value']
+    status = requests.get(f'{register_url}/UART_49_3_0_inp').json()['value']
+
+    cal = ctypes.c_short(cal).value
+    raw = ctypes.c_short(raw).value
+    return status, cal, raw
+
+
+def calibrate(scale, evok_baseurl):
+    input('Load to 0% and press Enter')
+    _, _, raw_zero = evok_read(evok_baseurl)
+    input('Load to 100% and press Enter')
+    _, _, raw_full = evok_read(evok_baseurl)
+    scale.calibrate(raw_zero, raw_full, full_cal_value)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Read from TE485 through EVOK.")
+    parser.add_argument('evok_baseurl', help="URL to evok")
+    args = parser.parse_args()
+
+    evok_baseurl = args.evok_baseurl
+
+    scale = Scale()
+    calibrate(scale, evok_baseurl)
+
+    while True:
+        status, cal, raw = evok_read(evok_baseurl)
+        load = scale.load(raw)
+        print(f'load: {load:.3f} status: {status:b} cal: {cal} raw: {raw}; '
+              f'{scale}')
+        print(('.' * 40) + ('|' * 40))
+        print('=' * int(40*load))
+        sleep(.1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pivovar/te485/te485_tool.py
@@ -0,0 +1,108 @@
+import argparse
+import ctypes
+import serial
+from pymodbus.client.sync import ModbusSerialClient
+from time import sleep
+
+'''
+This script helps with setting the te485.
+
+It can set communitcation from spinel to modbus. This is required to make it
+work with UniPI.
+'''
+
+
+class TE485Modbus(object):
+    def connect(self):
+        self.mb = ModbusSerialClient(
+            method='rtu', port='/dev/extcomm/0/0',
+            stopbits=1, bytesize=8, parity=serial.PARITY_NONE,
+            baudrate=19200, timeout=.1)
+
+    def set_sensitivity(self, sensitivity):
+        ''' Set the sensitivity of the load cell
+
+            0x00 ......... 2 mV/V
+            0x01 ......... 5 mV/V
+            0x02 ....... 10 mV/V
+
+            The greater the the sensitivity of the load cell means the
+            amplifier/adc has to be less sensitive and thus a same change will
+            cause smaller cange on the adc output.
+        '''
+        # Set sensitivity
+        self.mb.write_register(address=0, value=0xff, unit=0x31)
+        self.mb.write_register(address=16, value=sensitivity, unit=0x31)
+
+    def calibrate(self, raw_zero, raw_full, full_cal_value):
+        # Set upper limit value.
+        self.mb.write_register(address=0, value=0xff, unit=0x31)
+        self.mb.write_register(address=20, value=full_cal_value, unit=0x31)
+
+        # Calibrate zero
+        self.mb.write_register(address=0, value=0xff, unit=0x31)
+        self.mb.write_register(address=18, value=raw_zero, unit=0x31)
+
+        # Calibrate full.
+        self.mb.write_register(address=0, value=0xff, unit=0x31)
+        self.mb.write_register(address=19, value=raw_full, unit=0x31)
+        sleep(1)
+
+    def read(self):
+        r = self.mb.read_input_registers(0, count=3, unit=0x31)
+        status, cal, raw = r.registers
+        cal = ctypes.c_short(cal).value
+        raw = ctypes.c_short(raw).value
+        return status, cal, raw
+
+
+def set_te485_sensitivity():
+    parser = argparse.ArgumentParser(
+        description="Set the TE485 sensitivity through modbus.")
+    parser.add_argument('sensitivity', type=int, help="""
+            0 ......... 2 mV/V
+            1 ......... 5 mV/V
+            2 ....... 10 mV/V
+    """)
+    args = parser.parse_args()
+
+    te485 = TE485Modbus()
+    te485.set_sensitivity(args.sensitivity)
+
+
+def csume(s):
+    csum = 0
+    for b in s:
+        csum += b
+        yield b
+    yield 0xff & (255 - csum)
+    yield 0x0d
+
+
+def trans(s):
+    for w in s.split(' '):
+        yield int(w, 16)
+
+
+def swich_to_modbus():
+    sp = serial.Serial(
+        '/dev/extcomm/0/0', baudrate=9600, parity=serial.PARITY_NONE,
+        bytesize=8, stopbits=1, xonxoff=0, rtscts=0, timeout=1)
+
+    # Read identification on broadcast address.
+    sp.write(b'*B$?\r')
+    sp.read_until(b'\r')
+    # b'*B10TE485; v0672.01.07; f66 97; iBipolar\r'
+
+    # Enable writing command (the next one)
+    sp.write(b'*B1E\r')
+    sp.readline()
+    # b'*B10\r'
+
+    # Switch to Modbus
+    sp.write(bytes(csume(trans('2A 61 00 06 31 02 ed 02'))))
+    sp.readline()
+    # b'*a\x00\x051\x02\x00<\r'
+
+    # TODO switch also to 19200 bauds as that is the default UniPi rs485
+    # baudrate.
